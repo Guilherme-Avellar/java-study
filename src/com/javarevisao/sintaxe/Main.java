780af1720fa538f32a59bbefcf65c36273954b54
Guilherme <guilhermeavellar25@gmail.com>
2025-03-13 20:00:52 -0300

    Reository structure

---
+++ src/com/javarevisao/sintaxe/Main.java
@@ -0,0 +1,9 @@
+package com.javarevisao.sintaxe;
+
+public class Main {
+    public static void main(String[] args) {
+        // Exemplo de instanciação de obj
+        DeclaracaoSimplesObj obj1 = new DeclaracaoSimplesObj();
+        obj1.quantidadeInst();
+    }
+}
