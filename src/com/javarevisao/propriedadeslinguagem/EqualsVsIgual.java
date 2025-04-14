1b18c20444353d1c237315c8be686bbe62a66c5d
Guilherme <guilhermeavellar25@gmail.com>
2025-04-14 17:42:49 -0300

    Equals exaplanation

--- src/com/javarevisao/propriedadeslinguagem/EqualsVsIgual.java
+++ src/com/javarevisao/propriedadeslinguagem/EqualsVsIgual.java
@@ -16,16 +16,36 @@ public class EqualsVsIgual {
         *
         * Resumindo:
         * Se usa == para comparar valores de tipos primitivos e para verificar se dois objetos são
-        * a mesma instância na memória. Já .equals() deve ser usado para comparar o conteúdo de objetos.
-        *
+        * literalmente os mesmos na memória. Já .equals() deve ser usado para comparar o conteúdo dos objetos,
+        * se equals() for sobreescritos, com override.
+        * No caso de não sobrescrever o equals(), o Java ainda usa o comportamento padrão da classe Object,
+        * que também compara referências. Ou seja, se você não sobrescrever equals(), ele vai funcionar da
+        * mesma maneira que o ==.
+        * OBS: O overrride do equals é tão padrão que a IDE já faz altomáticamente se o programador desejar.
+        * Tipos Wrappers sobrescrevem o iquals() nativamente, para comparar valores, mas isso não é
+        * regra para todos os objetos e classe que você encontrar por aí.
         * */
 
+        // exemplo de funcionamento
         String obj1 = new String("Java");
         String obj2 = new String("Java");
+        // Observação para mecânica Java do tipo wrapper: se o valor passado for o mesmo, a classe economisa
+        // memória e coloca as instâncias de mesmo valor para referenciar-lo. Por isso eu não fiz da forma
+        // tradicional: String obj =  "Java"; Obriguei a criar duas instancias desta forma
 
         System.out.println("\nObjetos diferentes, mas instanciados da mesma classe, comparados com: ");
-        System.out.println(".quals() : " + obj1.equals(obj2));
-        System.out.println("== : " + (obj1 == obj2));
+        System.out.println(obj1 + ".quals() : " + obj1.equals(obj2));
+        System.out.println(obj2 + "== : " + (obj1 == obj2));
+
+        // Porém explicitamente atribuir um objeto a igualdade == será verdadeira
+        TiposWrappers tp = new TiposWrappers();
+        TiposWrappers tp2 = new TiposWrappers();
+        System.out.println("\nObjetos de mesma classe mas instancias diferentes, comparados com ==: " + (tp == tp2)); // false
+        // Agora quando atribuirmos a mesma instancia?
+        TiposWrappers tp3;
+        tp3 = tp;
+        System.out.println("\nObjetos de mesma classe e mesma instancia, comparados com ==: " + (tp == tp3));
+
 
     }
 
