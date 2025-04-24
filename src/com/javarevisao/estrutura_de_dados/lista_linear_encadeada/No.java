1635b2a27d93868cbf9c0a5e0f3a6d7616a54561
Guilherme <guilhermeavellar25@gmail.com>
2025-04-24 18:02:51 -0300

    Node to structure example

---
+++ src/com/javarevisao/estrutura_de_dados/lista_linear_encadeada/No.java
@@ -0,0 +1,12 @@
+package com.javarevisao.estrutura_de_dados.lista_linear_encadeada;
+
+// Exemplo de um nó (node em inglês) simples que pode ser reutilizado
+// para diversos tipos de estruturas
+
+public class No {
+    public int dado;
+    public No prox;
+}
+
+
+// Repare que prox é do tipo No também, usado para a referencia
