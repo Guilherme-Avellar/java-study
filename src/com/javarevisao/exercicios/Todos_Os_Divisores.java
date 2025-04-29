6f40ad04f7077ee3f9ec8638ad71ebac5a1e72d7
Guilherme <guilhermeavellar25@gmail.com>
2025-04-29 11:45:11 -0300

    Another exercise

---
+++ src/com/javarevisao/exercicios/Todos_Os_Divisores.java
@@ -0,0 +1,31 @@
+package com.javarevisao.exercicios;
+import java.util.Scanner;
+
+public class Todos_Os_Divisores {
+
+    private static void testarMultiplo(int number, int progrecao) {
+        for (int i = 1; i <= number; i += progrecao) {
+            if ((number % i) == 0) {
+                System.out.print(i + " ");
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int number = scanner.nextInt();
+
+        int multiplo = 0;
+
+        // aumenta a performace, pois um número par nunca será divisivel por um numero impar
+        if((number % 2) == 0){
+            testarMultiplo(number, 1);
+        }else {
+            testarMultiplo(number, 2);
+        }
+    }
+}
+
+// Exercicio do Neps Academy "Todos os Divisores"
