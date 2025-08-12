67f46c49eb25497084889495a857f049e0a289eb
Guilherme-Avellar <guilhermeavellar25@gmail.com>
2025-08-11 23:34:47 -0300

    feat: add fraction addition with LCM-based calculation and GCD simplification

--- src/com/javarevisao/exercicios/SomaDeFracoes.java
+++ src/com/javarevisao/exercicios/SomaDeFracoes.java
@@ -1,7 +1,5 @@
 package com.javarevisao.exercicios;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Scanner;
 
 // Exercicio 268 https://neps.academy/br/exercise/268
@@ -12,35 +10,75 @@ public class SomaDeFracoes {
 
         int A = scanner.nextInt();
         int B = scanner.nextInt();
+
         int C = scanner.nextInt();
         int D = scanner.nextInt();
 
-        int dividendo, divisor;
         // A/B + C/D
 
-        if (D == B) {
+        int dividendo, divisor;
+
+        if (B == D) {
+            dividendo = A + C;
+            divisor = D;
+        } else {
+            divisor = mmc(B, D);
+            A = (divisor / B) * A;
+            C = (divisor / D) * C;
             dividendo = A + C;
         }
 
-        List<Integer> divisores = new ArrayList<>();
-        int primo = 2;
-        boolean condicao = true;
-
-    }
-
-    private static int nextPrime(int x) {
-        x++;
-        for (int i = x; i >= 2; i--) {
-            if (x % i == 0) {
-                x++;
-                i = x;
-            } else if (i == 2) {
-                return x;
+        // simplificar
+        for (int i = 2; i <= divisor && i <= dividendo; i++) {
+            while(dividendo % i == 0 && divisor % i == 0) {
+                divisor /= i;
+                dividendo /= i;
             }
         }
 
-        return x;
+        System.out.print(dividendo + " " + divisor);
+    }
+
+    /**
+     * Metodo usado nas escolas de ensino fundamental
+     * @param B divisor da fração
+     * @param D divisor da outra fração
+     * @return retorna o minimo multiplo comum entre os dois números
+     */
+    private static int mmc(int B, int D) {
+        int primo = 2;
+        int minMultiplo = 1;
+
+        while ( (B >= primo) || (D >= primo) ) {
+            while (B % primo == 0 || D % primo == 0) {
+                minMultiplo *= primo;
+
+                if (B % primo == 0) {
+                    B /= primo;
+                }
+                if (D % primo == 0) {
+                    D /= primo;
+                }
+            }
+            primo = proxPrimo(primo);
+        }
+
+        return minMultiplo;
+    }
+
+    /**
+     * @param divisor número qualquer
+     * @return retorna o próximo número primo que seja maior que o número passado
+     */
+    private static int proxPrimo(int divisor) {
+        divisor++;
+        for (int i = divisor - 1; i >= 2; i--) {
+            if (divisor % i == 0) {
+                divisor++;
+                i = divisor;
+            }
+        }
+
+        return divisor;
     }
 }
-
-
